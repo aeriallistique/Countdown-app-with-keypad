833577a0bdc3e81df78c6c17a7ecdf136be23168
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2020-10-04 12:56:24 +0100

    fixed responsivnes
    made it responsive on all browsers and fixed the crowing sound issue on Safari

--- app.js
+++ app.js
@@ -9,20 +9,30 @@ const cock = document.querySelector('.cock');
 var x = document.getElementById("myAudio"); 
 var timesPlayed = 0;
 max_plays = 2;
-
 let str;
 let SECONDS = null;
+
+function unlockAudio() {
+    const sound = new Audio('crowing.mp3');
+
+    sound.play();
+    sound.pause();
+    sound.currentTime = 0;
+
+    document.body.removeEventListener('click', unlockAudio)
+    document.body.removeEventListener('touchstart', unlockAudio)
+}
+document.body.addEventListener('click', unlockAudio);
+document.body.addEventListener('touchstart', unlockAudio);
 
 // event listener for numbers
 numbers.addEventListener('click', (e)=>{
    if(e.target.tagName == 'DIV'){ return }
-    
     if(time.innerHTML.length == 0 || time.innerHTML.length == 1 ){
         let gTime = time.innerHTML + e.target.dataset.numb;
         let splitTime = gTime.split(':');
         SECONDS = (Number(splitTime));
     }
-
     if(time.innerHTML.length == 2){
        let fCut =time.innerHTML.split('');
        let sCut = [fCut[0], ':', fCut[1]];
@@ -41,7 +51,6 @@ numbers.addEventListener('click', (e)=>{
         splitTime = gTime.split(':');
         SECONDS = (Number(splitTime[0]) * 60) + Number(splitTime[1]);
     }
-  
     if( time.innerHTML.length == 5){
         let fCut =time.innerHTML.split('');
         let sCut = [fCut[0],':', fCut[1],fCut[3], ':' , fCut[4] ];
@@ -60,18 +69,14 @@ numbers.addEventListener('click', (e)=>{
         splitTime = gTime.split(':');
         SECONDS = Number(splitTime[0] * 3600) + Number(splitTime[1] * 60) + Number(splitTime[2]);
     }
-
     if(time.innerHTML.length < 8){
         time.innerHTML = `${time.innerHTML}${e.target.dataset.numb}`;
         str = time.innerHTML.toString()
         return str;
-    } 
-    
+    }  
     return str, SECONDS; 
 });
 
-
-// make the cock sing :)
 function playAudio (){
     x.play();
     timesPlayed++;
@@ -93,10 +98,9 @@ buttons.addEventListener('click', (e)=>{
         time.innerHTML = '';
         SECONDS = 0;
         set.classList.remove('hide');
-        cock.classList.remove('move_cock');
+        cock.classList.remove('move_cock')
     }
     if(e.target.classList == 'set' && SECONDS){
-        
         stop.classList.remove('hide');
         clear.classList.add('hide');
         set.classList.add('hide');
@@ -107,20 +111,18 @@ buttons.addEventListener('click', (e)=>{
                 stop.classList.add('hide');
                 clear.classList.remove('hide');
                 time.innerHTML = `00s`;
-                playAudio();
                 cock.classList.add('move_cock');
-                cock.src= "./Animated_Cockerel 2.gif?"+new Date().getTime();
-
-            
+                cock.src = "./img/Animated_Cockerel 2.gif?"+new Date().getTime();
+                playAudio();
             }
             let minute = Math.floor(SECONDS / 60) % 60;
-                minute = minute < 10 ? `0${minute}` : minute;
+            minute = minute < 10 ? `0${minute}` : minute;
             let secunde = SECONDS % 60;
-                secunde = secunde < 10 ? `0${secunde}` : secunde;
+            secunde = secunde < 10 ? `0${secunde}` : secunde;
             let ore = Math.floor((SECONDS / 60) / 60);
-                ore = ore < 10 ? `0${ore}` : ore;
+            ore = ore < 10 ? `0${ore}` : ore;
         
-            time.innerHTML = `${ore}hr ${minute}min ${secunde}s`;
+            time.innerHTML = `${ore}h ${minute}m ${secunde}s`;
             SECONDS--;
         }, 1000)
         
@@ -131,7 +133,5 @@ buttons.addEventListener('click', (e)=>{
             clearInterval(myVar);
           
         } );
-
     }
-
 });
